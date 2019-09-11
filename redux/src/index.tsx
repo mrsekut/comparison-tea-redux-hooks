d95549c020a06f0104133e680cdf9663303c2891
mrsekut <k.cloudspider@gmail.com>
2019-09-12 03:22:34 +0900

    [Add]todo with redux

--- redux/src/index.tsx
+++ redux/src/index.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
